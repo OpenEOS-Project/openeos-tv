176294287bd7860d8edf1d62f2e9fccf42cf2424
hermanntoast <lukas.spitznagel@netzint.de>
2026-01-28 16:07:53 +0100

    Fix worklow

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,17 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  * Metro configuration
  * https://reactnative.dev/docs/metro
  */
-const config = {};
+const config = {
+  resolver: {
+    // Block Node.js built-in modules that don't exist in React Native
+    resolveRequest: (context, moduleName, platform) => {
+      // Redirect axios to use browser build
+      if (moduleName === 'axios') {
+        return context.resolveRequest(context, 'axios/dist/browser/axios.cjs', platform);
+      }
+      return context.resolveRequest(context, moduleName, platform);
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
